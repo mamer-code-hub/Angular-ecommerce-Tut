a7263c34de6ee0ec4f06b84e66dee0c450192efa
mamer-code-hub <moataz.amer2@gmail.com>
2025-02-22 19:19:28 +0200

    add news bar to cart

--- src/app/features/products/components/product-card/product-card.component.ts
+++ src/app/features/products/components/product-card/product-card.component.ts
@@ -15,13 +15,50 @@ export class ProductCardComponent {
   @Output() addToCart = new EventEmitter<string>();
   @Output() addToWish = new EventEmitter<string>();
 
+  newsItems = [
+    { text: "New Sale Starting Soon!", icon: "fa-solid fa-gift" },
+    { text: "Limited Stock, Hurry Up!", icon: "fa-solid fa-exclamation-circle" },
+    { text: "Special Discount Today Only!", icon: "fa-solid fa-percent" },
+    { text: "Exclusive Offer for You!", icon: "fa-solid fa-star" },
+    { text: "Don't Miss Our Latest Products!", icon: "fa-solid fa-box" }
+  ];
+
+  randomNewsItems: Array<{ text: string, icon: string }> = [];
+  
+  // Declare intervalId to keep track of the interval
+  intervalId: any;
+
+  ngOnInit() {
+    this.generateRandomNews();
+    this.startNewsInterval();
+  }
+
+  ngOnDestroy() {
+    // Clean up interval when the component is destroyed
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
+    }
+  }
+
+  startNewsInterval() {
+    this.intervalId = setInterval(() => {
+      this.generateRandomNews();
+    }, 5000); // Change news every 5 seconds
+  }
+
+  generateRandomNews() {
+    this.randomNewsItems = [];
+    const randomIndexes = Array.from({ length: 3 }, () => Math.floor(Math.random() * this.newsItems.length));
+    randomIndexes.forEach(index => {
+      this.randomNewsItems.push(this.newsItems[index]);
+    });
+  }
+
   onAddToCart() {
     this.addToCart.emit(this.product._id);
   }
   onAddToWish() {
     this.addToWish.emit(this.product._id);
   }
-
-
 
 }
